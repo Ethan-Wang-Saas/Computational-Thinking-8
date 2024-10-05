718dbc5522bad7a4e0a18b3504f4c00222f435ee
Ethan-Wang-Saas <ethanwang@seattleacademy.org>
2024-10-05 02:52:32 +0000

    code art submission

---
+++ Projects/codeart.py
@@ -0,0 +1,71 @@
+import turtle 
+
+john= turtle.Turtle()
+john.penup()
+john.goto(175,-10)
+john.color("red")
+john.pendown()
+john.speed(10000)
+
+jim= turtle.Turtle()
+jim.penup()
+jim.goto(-235,-10)
+jim.color("red")
+jim.pendown()
+jim.speed(10000)
+
+billy= turtle.Turtle()
+billy.penup()
+billy.goto(210,0)
+billy.color("Orange")
+billy.pendown()
+billy.speed(10000)
+
+t= turtle.Turtle()
+t. penup()
+t.goto(-200,0)
+t.pendown()
+t.speed(10000)
+
+
+for i in range(500):
+    t.forward(50+i)
+    t.right(11+1)
+    t.color("brown")
+    t.forward(12+i)
+    t.right(140+1)
+    t.color("Orange")
+    t.circle(2+i)
+    t.color("green")
+    t.forward(50+i)
+    t.right(32+1)
+    t.color("blue")
+    t.circle(10)
+    t.color("purple")
+
+    billy.forward(50+i)
+    billy.right(11+1)
+    billy.color("brown")
+    billy.forward(12+i)
+    billy.right(140+1)
+    billy.color("Orange")
+    billy.circle(2+i)
+    billy.begin_fill
+    billy.forward(10)
+    billy.end_fill
+    billy.color("green")
+    billy.forward(50+i)
+    billy.right(32+1)
+    billy.color("blue")
+    billy.circle(10)
+    billy.color("Purple")
+
+    john.forward(100)
+    john.left(191)
+
+    jim.forward(100)
+    jim.left(191)
+turtle.exitonclick()
+
+
+
---
+++ Projects/stickmen.py
@@ -0,0 +1,54 @@
+import turtle
+
+# Create a turtle object
+pen = turtle.Turtle()
+pen.speed(10)  
+
+# stickmen place
+def draw_stickman(x, y):
+    pen.penup()
+    pen.goto(x, y)
+    pen.pendown()
+
+    # Head
+    pen.circle(20)
+
+    # Body
+    pen.penup()
+    pen.goto(x, y - 40)
+    pen.pendown()
+    pen.goto(x, y - 100)
+
+    # Left arm
+    pen.penup()
+    pen.goto(x, y - 50)
+    pen.pendown()
+    pen.goto(x - 40, y - 70)
+
+    # Right arm
+    pen.penup()
+    pen.goto(x, y - 50)
+    pen.pendown()
+    pen.goto(x + 40, y - 70)
+
+    # Left leg
+    pen.penup()
+    pen.goto(x, y - 100)
+    pen.pendown()
+    pen.goto(x - 30, y - 150)
+
+    # Right leg
+    pen.penup()
+    pen.goto(x, y - 100)
+    pen.pendown()
+    pen.goto(x + 30, y - 150)
+
+# Draw two stickmen at different positions
+draw_stickman(-100, 100)  # Stickman 1
+draw_stickman(100, 100)   # Stickman 2
+
+# Hide the turtle
+pen.hideturtle()
+
+# Finish the drawing
+turtle.done()
